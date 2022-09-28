cfa90d4b73e535a2098c913eb41a7f5afc3505ca
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-09-28 11:20:17 -0300

    Subindo a aplicação configurada

---
+++ src/main/java/io/luaprogrammer/ifooddevweek_springboot/IFoodDevWeekSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.luaprogrammer.ifooddevweek_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IFoodDevWeekSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IFoodDevWeekSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/luaprogrammer/ifooddevweek_springboot/IFoodDevWeekSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.luaprogrammer.ifooddevweek_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IFoodDevWeekSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
